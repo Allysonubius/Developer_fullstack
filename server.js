47b3430cdf7acb2f00f151613ff755c1eafdba5e
Allysonubius <allysomoliveira7@gmail.com>
2020-07-18 20:50:12 -0300

    Realizado tratativa de errors ao uso da API na porta 3000.

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const route = router.get('/', (req, res, next) => {
 app.use('/', route);
 
 server.listen(port);
+server.on('error', onError);
 console.log('API rodando na porta localhost:' + port);
 
 function normalizePort(val) {
@@ -32,4 +33,28 @@ function normalizePort(val) {
         return port;
     }
     return false;
+}
+
+function onError(error) {
+    if (error.syscall !== 'liste') {
+        throw error;
+    }
+
+    const bind = typeof port === 'string' ?
+        'Pipe ' + port :
+        'Port' + port;
+
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + 'Requer privilegios elevados de acesso !');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + 'No momento esta sendo usado !');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+
 }
