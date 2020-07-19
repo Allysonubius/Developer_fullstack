fee1f1d6d8e0b620206e81daf1fc60f91f44a4d6
Allysonubius <allysomoliveira7@gmail.com>
2020-07-19 14:49:35 -0300

    Realizado instalação mongose para trabalhar com MongoDB.

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,22 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const controller = require('../controllers/product-controller');
 
+router.post('/', controller.post);
 
-router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
+router.put('/:id', controller.put);
 
-router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id,
-        item: req.body
-    });
-});
-
-router.delete('/', (req, res, next) => {
-    res.status(200).send(req.body);
-});
+router.delete('/', controller.delete);
 
 
 
